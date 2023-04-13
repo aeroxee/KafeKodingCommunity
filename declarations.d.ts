ec1b7842315c859655a62855ed927e8dd86419f6
fajhri <vipercode305@gmail.com>
2023-04-14 04:29:11 +0700

    declare module import for typescript

---
+++ declarations.d.ts
@@ -0,0 +1,27 @@
+declare module "*.jpg" {
+  import { ImageSourcePropType } from "react-native/types";
+  const content: ImageSourcePropType;
+
+  export default content;
+}
+
+declare module "*.png" {
+  import { ImageSourcePropType } from "react-native/types";
+  const content: ImageSourcePropType;
+
+  export default content;
+}
+
+declare module "*.jpeg" {
+  import { ImageSourcePropType } from "react-native/types";
+  const content: ImageSourcePropType;
+
+  export default content;
+}
+
+declare module "*.gif" {
+  import { ImageSourcePropType } from "react-native/types";
+  const content: ImageSourcePropType;
+
+  export default content;
+}
