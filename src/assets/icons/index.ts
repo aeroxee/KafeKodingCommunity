e0fbef518ea1fcb85860ea2223c5ae28ccaebbc3
fajhri <vipercode305@gmail.com>
2023-04-14 04:35:13 +0700

    Assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+import homePic from "./home.png";
+import homeActivePic from "./homeActive.png";
+import searchPic from "./search.png";
+import searchActivePic from "./serachActive.png";
+import explorePic from "./explore.png";
+import exploreActivePic from "./exploreActive.png";
+
+export { homePic, homeActivePic, searchPic, searchActivePic, explorePic, exploreActivePic };
